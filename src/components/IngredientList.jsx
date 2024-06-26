4ec1cd1147ac0882bf8b5ee04e52db33032f3d06
jcao91 <jcao91@yahoo.com>
2024-06-26 15:50:31 -0500

    added addToBurger button

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -5,6 +5,7 @@ const IngredientList = (props) => {
       {props.ingredients.map((ingredient, index) => (
         <li key={index} style={{ backgroundColor: ingredient.color }}>
           {ingredient.name}
+          <button onClick={() => props.addToBurger(ingredient)}>+</button>
         </li>
       ))}
     </ul>
